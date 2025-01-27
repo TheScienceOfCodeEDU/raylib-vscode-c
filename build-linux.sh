84bd5b306cd72b89fcde8e611e4cefee330d8c7a
dacanizares <dacanizares@outlook.com>
2025-01-26 23:28:50 -0500

    Added run linux
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>

--- build-linux.sh
+++ build-linux.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+
 mkdir -p build/linux
-cp /usr/local/lib64/libraylib.so.5.5.0 ./
-mv ./libraylib.so.5.5.0 ./libraylib.so.550
+cp /usr/local/lib64/libraylib.so.5.5.0 ./build/linux
+mv ./build/linux/libraylib.so.5.5.0 ./build/linux/libraylib.so.550
 gcc main.c -g -ggdb -lraylib  -o build/linux/main -std=c99 -pedantic
