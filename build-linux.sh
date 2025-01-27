4af04b87ba2b2f0ee17f42693d0733302d143199
dacanizares <dacanizares@outlook.com>
2025-01-26 23:07:50 -0500

    Fixed linux launch
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>

--- build-linux.sh
+++ build-linux.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 mkdir -p build/linux
-gcc main.c -g -ggdb -lraylib -o build/linux/main -std=c99 -pedantic
+cp /usr/local/lib64/libraylib.so.5.5.0 ./
+mv ./libraylib.so.5.5.0 ./libraylib.so.550
+gcc main.c -g -ggdb -lraylib  -o build/linux/main -std=c99 -pedantic
