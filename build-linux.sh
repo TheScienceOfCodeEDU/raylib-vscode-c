488e6fa1aa1d74775767edf6bc97d2bfc443e789
dacanizares <dacanizares@outlook.com>
2025-01-28 17:05:49 -0500

    Improved linux launch

--- build-linux.sh
+++ build-linux.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 mkdir -p build/linux
-cp /usr/local/lib64/libraylib.so.5.5.0 ./build/linux
-mv ./build/linux/libraylib.so.5.5.0 ./build/linux/libraylib.so.550
 gcc main.c -g -ggdb -lraylib  -o build/linux/main -std=c99 -pedantic
