1430ea90611774df70c748f4f59116480914c863
dacanizares <dacanizares@outlook.com>
2025-04-14 22:43:45 -0500

    Updated win build to use raylib.dll
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>

--- build-win.bat
+++ build-win.bat
@@ -1,3 +1,4 @@
 @echo off
 mkdir "build/win" 2>NUL
-gcc main.c -IC:\raylib\include -LC:\raylib\lib -g -ggdb -lraylib -lgdi32 -lwinmm -o build/win/main.exe -std=c99 -pedantic
+copy "C:\raylib\lib\raylib.dll" "build/win" >NUL
+gcc main.c -IC:\raylib\include -Lbuild/win -g -ggdb -lraylib -lgdi32 -lwinmm -o build/win/main.exe -std=c99 -pedantic
